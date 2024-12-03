a572901088a59f7ae066a6c89d33cf53db2efbec
CNicer <nicerx@qq.com>
2024-12-03 19:55:54 +0800

    Close outputchannel show

--- src/output/output.ts
+++ src/output/output.ts
@@ -4,5 +4,5 @@ export const output_channel = vscode.window.createOutputChannel('XP4Helper')
 
 export function xp4Log(str:string) {
     output_channel.append(str + "\n")
-    output_channel.show()
+    // output_channel.show(false)
 }
